c8248fb8bd59ab27fad40013d85a3f7752da74e3
Tibor Sohonyai <185492254+Rocka06@users.noreply.github.com>
2026-04-24 16:38:37 +0200

    upload project

---
+++ TicketDownloader.js
@@ -0,0 +1,42 @@
+const { chromium } = require('playwright');
+const fs = require('fs');
+require('dotenv').config();
+
+(async () => {
+    const browser = await chromium.launch({ headless: false });
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    const fileContent = fs.readFileSync(process.env.ISSUE_ID_CSV_PATH, 'utf-8');
+    const tickets = fileContent.split(process.env.CSV_EOF_CHAR);
+
+    await page.goto(`${process.env.EASYPROJECT_BASE_URL}/login`);
+
+    console.log('Please log in manually');
+
+    await page.waitForURL("**/my/page", { timeout: 0 });
+
+    tickets.map((ticket) => {
+        const id = ticket.trim();
+
+        console.log(`Downloading ticket ${id}`);
+        await page.goto(`${process.env.EASYPROJECT_BASE_URL}/issues/${id}`, { waitUntil: 'networkidle' });
+
+        const button = page.locator('#load-more-easy-journal-history');
+        if (await button.count() !== 0) {
+            await button.click();
+            await page.waitForLoadState('networkidle');
+        }
+
+        await page.pdf({
+            path: `${process.env.PDF_SAVE_PATH}/Ticket-${id}.pdf`,
+            format: 'A4',
+            printBackground: false,
+            displayHeaderFooter: true
+        });
+
+        console.log(`Saved Ticket-${id}.pdf`);
+    });
+
+    await browser.close();
+})();   
